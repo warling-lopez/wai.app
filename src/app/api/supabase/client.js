025a330296d278f5dfdf00d2913d21ba2f94eb8c
warling <warlinglopez01@gmail.com>
2025-04-26 15:52:23 -0400

    ver

--- src/app/api/supabase/client.js
+++ src/app/api/supabase/client.js
@@ -1,7 +1,6 @@
-/*import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-    import.meta.env.SUPABASE_URL, 
-    import.meta.env.SUPABASE_ANON_KEY
+    progress.env.SUPABASE_URL, 
+    progress.env.SUPABASE_KEY
 );
-*/
---
+++ src/app/log/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Login from './login'
+function page() {
+  return (<>
+    <div>page</div>
+    <Login/>
+  </>)
+}
+
+export default page
