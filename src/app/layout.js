44d486c8759f991d511890cafa14611207e46592
warling <warlinglopez01@gmail.com>
2025-05-04 15:17:08 -0400

    bug impt

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export  default async function RootLayout({ children }) {
  
   return (
     <html lang="en">
-      <body className={roboto.className }>
+      <body className={roboto.className}>
         <SidebarProvider defaultOpen={defaultOpen}>
           <AppSidebar/>
           <main className="w-full grid-rows-2">
