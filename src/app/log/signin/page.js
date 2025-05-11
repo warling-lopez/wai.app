11225735f079b57ce4e64b269e0e9464ee458cce
warling <warlinglopez01@gmail.com>
2025-05-11 12:32:56 -0400

    think

--- src/app/log/signin/page.js
+++ src/app/log/signin/page.js
@@ -1,11 +1,11 @@
 "use client";
-import SignUp from "@/components/Auth/SignUp";
+import SignIn from "@/components/Auth/SignIn";
 
-export default function SignIn() {
+export default function signIn() {
   return (
     <div className="flex w-full min-h-svh items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <SignUp />
+        <SignIn />
       </div>
     </div>
   );
--- src/app/log/signup/page.js
+++ src/app/log/signup/page.js
@@ -1,12 +1,12 @@
 "use client";
 
-import SignIn from "@/components/Auth/SignIn";
+import SignUp from "@/components/Auth/SignUp";
 
-export default function SignUp() {
+export default function signUp() {
   return (
     <div className="flex w-full min-h-svh items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <SignIn />
+        <SignUp />
       </div>
     </div>
   );
