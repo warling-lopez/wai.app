4f9b3c9aea7addf44081fdbafeab59c1985aa4a2
warling <warlinglopez01@gmail.com>
2025-05-11 12:41:04 -0400

    all list

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,35 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React from "react";
 import { Supabase } from "@/supabase/supabase";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
-function page() {
+function Page() {
   const direction = useRouter();
 
-  useEffect(()=>{
-    Supabase.auth.onAuthStateChange((event, session)=>{
-      if(!session){
-         direction.push("/IA")
+  useEffect(() => {
+    Supabase.auth.onAuthStateChange((event, session) => {
+      if (!session) {
+        direction.push("/IA");
       }
-    })
-  },[])
+    });
+  }, [direction]);
+  
   return (
     <div className="w-[100vh] h-[95vh] flex justify-center align-center rounded-2xl bg-secondary">
       <Link href="/IA">
-        <img src="/Wally.png" className="animationForIAWallyImg" />
+        <Image
+          src="/Wally.png"
+          alt="Imagen de Wally"
+          className="animationForIAWallyImg"
+          width={300}
+          height={300}
+        />
       </Link>
     </div>
   );
 }
 
-export default page;
+export default Page;
