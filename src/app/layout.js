938e23f33e9603fd944b4790db3050ca23404d78
warling <warlinglopez01@gmail.com>
2025-05-21 13:09:45 -0400

    prueva no sustentable

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,52 @@
-import "./globals.css";
-import { Roboto } from "next/font/google";
+// app/layout.jsx
+'use client';
 
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"], // selecciona los pesos que necesites
-});
-export const metadata = {
-  title: "WALLY",
-  description: "UN AGENT UNIQUE",
-};
-export default async function RootLayout({ children }) {
+import { useEffect } from "react";
+import { Supabase } from "@/supabase/supabase";
+import { useUserStore } from "@/store/useUserStore";
+import "./globals.css"
+export default function RootLayout({ children }) {
+  const setUser = useUserStore(state => state.setUser);
+
+  useEffect(() => {
+    const cargarUsuario = async () => {
+      const {
+        data: { user },
+      } = await Supabase.auth.getUser();
+
+      if (user) {
+        const correo = user.email;
+        let avatar = null;
+
+        const { data: perfil } = await Supabase
+          .from("perfiles")
+          .select("foto")
+          .eq("correo", correo)
+          .single();
+
+        if (perfil?.foto) {
+          avatar = perfil.foto;
+        } else if (user.app_metadata?.provider === "google") {
+          avatar = user.user_metadata?.avatar_url;
+        } else {
+          avatar = `https://api.dicebear.com/7.x/pixel-art/svg?seed=${correo}`;
+        }
+
+        setUser({
+          correo,
+          avatar,
+          nombre: user.user_metadata?.name || "Usuario",
+        });
+      }
+    };
+
+    cargarUsuario();
+  }, [setUser]);
+
   return (
-    <html lang="en">
-      <body className={roboto.className}>
-        <div className="m-2">{children}</div>
+    <html>
+      <body>{children}
+        <div>hola {setUser}</div>
       </body>
     </html>
   );
---
+++ src/store/useUserStore.js
@@ -0,0 +1,9 @@
+// src/store/useUserStore.js
+import { create } from 'zustand';
+
+export const useUserStore = create((set) => ({
+  user: null,
+  setUser: (userData) => set({ user: userData }),
+  clearUser: () => set({ user: null }),
+}));
+
