664de098383d1b70206e7b75fe54ff74cb99affd
warling <warlinglopez01@gmail.com>
2025-08-03 17:08:26 -0400

    format

--- src/app/api/send-msg-supabase/route.js
+++ src/app/api/send-msg-supabase/route.js
@@ -3,15 +3,15 @@ import { Supabase } from "@/Supabase/Supabase";
 
 export async function POST(request) {
   const body = await request.json();
-  const { Chat_id, role, content } = body;
+  const { Chat_id, role, content, user_id } = body;
 
-  if (!Chat_id || !role || !content) {
+  if (!Chat_id || !role || !content || user_id) {
     return new Response(JSON.stringify({ error: "Missing data" }), { status: 400 });
   }
 
   const { data, error } = await Supabase
     .from("msg")
-    .insert([{ Chat_id, role, content }]);
+    .insert([{ Chat_id, role, content, user_id }]);
 
   if (error) {
     return new Response(JSON.stringify({ error: "Database error" }), { status: 500 });
