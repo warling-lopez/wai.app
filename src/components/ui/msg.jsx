e3148b68cd9d784b9eb58f2077286ab610fc7587
warling <warlinglopez01@gmail.com>
2025-08-07 05:29:01 -0400

    herramientas de codigo

--- src/components/ui/msg.jsx
+++ src/components/ui/msg.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useRef } from "react";
 import { cn } from "@/lib/utils"; // o usa clsx si no tienes cn
 import ReactMarkdown from "react-markdown";
+import Prism from "prismjs";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-css";
+
+import "prismjs/themes/prism-tomorrow.css";
+import "@/components/css/prism-custom.css";
+
 
 const Msg = ({ role, content }) => {
   const isUser = role === "user";
   const contentRef = useRef(null);
-
+  Prism.highlightAll();
   return (
     <div
       className={cn(
