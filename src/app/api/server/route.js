c04249f16671ed38c69afd23bbe7e381e9943ae5
warling <warlinglopez01@gmail.com>
2025-08-20 00:46:40 -0400

    finish to test

--- src/app/api/server/route.js
+++ src/app/api/server/route.js
@@ -45,7 +45,7 @@ export async function POST(request) {
 
         // --- Paso 3: Enviar el contexto a OpenAI ---
         const completion = await openai.chat.completions.create({
-          model: "gpt-4o-mini-2024-07-18",
+          model: "gpt-5-mini",
           messages: chatHistoryForAI,
           functions,
           function_call: "auto",
